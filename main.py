1ff19c78bbaa70a714718c416668ad9ec9761088
lorenkim <lkim1@loyola.edu>
2026-04-06 09:25:24 -0400

    data loader

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
 # Loren Kim
 # main.py for CS496 Research
 
+import os
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+from PIL import Image
+from tqdm import tqdm
 
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
 
+import torchvision.transforms as transforms
+import torchvision.models as models
+
+from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score
+from sklearn.model_selection import train_test_split
+
+# import dataset
+class FractureDataset(Dataset):
+    def __init__(self, df, transform=None):
+        self.df = df
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        img_path = self.df.iloc[idx]['image_path']
+        image = Image.open(img_path).convert("RGB")
+
+        fracture = self.df.iloc[idx]['fracture']
+        fracture_type = self.df.iloc[idx]['fracture_type']
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, torch.tensor(fracture).long(), torch.tensor(fracture_type).long()
+    
 # DATA PREPROCESSING
 
 # MODEL SELECTION
